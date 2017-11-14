1ca9c7b2523fbbf7da4b03d5bb8df4180c6a90e3
hllele <958037237@qq.com>
2017-11-14 13:22:38 +0800

    commit base

---
+++ app/src/main/java/com/llele/mvpmaster/base/BaseActivity.java
@@ -0,0 +1,101 @@
+package com.llele.mvpmaster.base;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.llele.mvpmaster.R;
+import com.llele.mvpmaster.utils.ActivityManager;
+import com.llele.mvpmaster.utils.StatusBarSetting;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+/**
+ * Author： huanglele on 2017/11/10.
+ */
+
+public abstract class BaseActivity extends SwipeBackActivity {
+    public Context mContext;
+    private SwipeBackLayout mSwipeBackLayout;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        mContext = this;
+        this.initPresenter();
+        this.initView();
+
+    }
+
+
+
+    public abstract int getLayoutId();
+    public abstract void initPresenter();
+    public abstract void initView();
+
+    /**
+     * 是否滑动返回
+     * @param  isSwipeBack
+     */
+    public void isSwipeBack(boolean isSwipeBack){
+        setSwipeBackEnable(isSwipeBack);
+        mSwipeBackLayout = getSwipeBackLayout();
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
+    }
+
+    public void startActivity(Class<?> cls){
+        startActivity(cls,null);
+    }
+
+    public void startActivityForResult(Class<?> cls,int requestCode){
+        startActivityForResult(cls,null,requestCode);
+    }
+
+    public void startActivityForResult(Class<?> cls,Bundle bundle,int requestCode){
+        Intent intent = new Intent();
+        intent.setClass(this,cls);
+        if (bundle != null){
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent,requestCode);
+    }
+
+    public void startActivity(Class<?> cls,Bundle bundle){
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityManager.getAppManager().finishActivity(this);
+    }
+}
---
+++ app/src/main/java/com/llele/mvpmaster/base/BaseFragment.java
@@ -0,0 +1,86 @@
+package com.llele.mvpmaster.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Author： huanglele on 2017/11/10.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (getContentViewLayoutID() != 0) {
+            return inflater.inflate(getContentViewLayoutID(), null);
+        } else {
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+    protected abstract int getContentViewLayoutID();
+
+    protected abstract void initViews(View view);
+
+    private boolean isFirstVisible = true;
+    private boolean isFirstInvisible = true;
+    private boolean isPrepared;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initPrepare();
+    }
+
+    private synchronized void initPrepare() {
+        if (isPrepared) {
+            onFirstUserVisible();
+        } else {
+            isPrepared = true;
+        }
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            if (isFirstVisible) {
+                isFirstVisible = false;
+                initPrepare();
+            } else {
+                onUserVisible();
+            }
+        } else {
+            if (isFirstInvisible) {
+                isFirstInvisible = false;
+                onFirstUserInvisible();
+            } else {
+                onUserInvisible();
+            }
+        }
+    }
+
+    protected abstract void onFirstUserVisible();
+    protected abstract void onUserVisible();
+    private void onFirstUserInvisible() { }
+    protected abstract void onUserInvisible();
+
+    @Override
+    public void onDestroy() {
+        DestroyViewAndThing();
+        super.onDestroy();
+    }
+
+    protected abstract void DestroyViewAndThing();
+}
