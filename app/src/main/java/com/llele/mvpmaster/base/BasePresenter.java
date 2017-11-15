22ce7035239087be3cbad849830156111a40b7cb
hllele <958037237@qq.com>
2017-11-15 10:02:14 +0800

    commit utils

---
+++ app/src/main/java/com/llele/mvpmaster/base/BasePresenter.java
@@ -0,0 +1,13 @@
+package com.llele.mvpmaster.base;
+
+/**
+ * Author： huanglele on 2017/11/10.
+ */
+
+public interface BasePresenter {
+    void showDialog();
+
+    void dismissDialog();
+
+    void showToast();
+}
